e44afe59eb14a280e936c23460cd44a97bf5c119
Rui Vilela <ruivil@gmail.com>
2022-08-19 12:34:44 +0100

    .

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,28 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 // import * as moment from 'moment';
 import * as moment from 'moment-timezone';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent implements OnInit {
   title = 'app';
   today: Date = new Date();
   pipe = new DatePipe('en-US');
   todayWithPipe = null;
+
+  @HostListener('window:beforeunload', ['$event'])
+  beforeunloadHandler($event): void {
+      $event.preventDefault()
+      $event.returnValue = false;
+
+  }
+
   ngOnInit(): void {
     this.todayWithPipe = this.pipe.transform(Date.now(), 'full');
     console.log(Intl.DateTimeFormat().resolvedOptions());
     console.log(moment().tz('America/New_York').format('MMMM Do YYYY, h:mm:ss a'));
-    console.log(moment.version);
-    console.log(moment.defaultFormatUtc)
+    // console.log(moment.version);
+    // console.log(moment.defaultFormatUtc)
     console.log(moment.tz.guess());
-    console.log(moment.tz.zonesForCountry('US'));
-    console.log(moment.tz.zonesForCountry('PT'));
-    console.log(navigator.userAgent)
+    // console.log(moment.tz.zonesForCountry('US'));
+    // console.log(moment.tz.zonesForCountry('PT'));
+    console.log(navigator.userAgent);
   }
 }
+
+
